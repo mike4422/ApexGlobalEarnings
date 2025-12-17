58bed69bdbbf72c0ff299f971977a8461d674194
mike clinton <mikeclinton508@gmail.com>
2025-12-17 10:02:54 +0100

    added the tawk.to support widget

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { ReactNode } from "react";
 import RootShell from "@/components/layout/RootShell";
+import Script from "next/script";
 
 export const metadata = {
   title: "ApexGlobalEarnings | Smart Trading & Investment",
@@ -13,6 +14,22 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       {/* ❗ No bg-bg here anymore – each shell controls its own background */}
       <body className="antialiased">
         <RootShell>{children}</RootShell>
+
+        {/*Start of Tawk.to Script*/}
+        <Script id="tawk-to" strategy="afterInteractive">
+          {`
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/694265f5131715197aa70c78/1jcllsiap';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+            })();
+          `}
+        </Script>
+        {/*End of Tawk.to Script*/}
       </body>
     </html>
   );
