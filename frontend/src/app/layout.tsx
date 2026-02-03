7a174d51a837fbccc93c49971c83a6294e78b947
mike clinton <mikeclinton508@gmail.com>
2026-02-03 13:52:03 +0100

    change the tawk.to url

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -36,3 +36,18 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     </html>
   );
 }
+
+
+// <!--Start of Tawk.to Script-->
+// <script type="text/javascript">
+// var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+// (function(){
+// var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+// s1.async=true;
+// s1.src='https://embed.tawk.to/6981ed806273081c3ac0d253/1jghofga9';
+// s1.charset='UTF-8';
+// s1.setAttribute('crossorigin','*');
+// s0.parentNode.insertBefore(s1,s0);
+// })();
+// </script>
+// <!--End of Tawk.to Script-->
